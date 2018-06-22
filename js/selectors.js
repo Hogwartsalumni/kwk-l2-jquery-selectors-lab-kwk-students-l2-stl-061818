2b7f745ddb67ae1d34e9a40415b914e5f8ce56e2
Scarlett Patton <scarwars05@gmail.com>
2018-06-22 18:14:04 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -5,3 +5,7 @@ function h1Selector(element) {
   document.querySelectorAll('h1');
   return document.querySelectorAll('h1');
 }
+
+function liInOlSelector(element) {
+  document.querySelectorAll('ol)
+}
